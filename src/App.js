9349199998a66aabeb4c23f7d9c99d42a34f724e
M Groesbeck <m.groesbeck@gmail.com>
2020-02-20 13:50:30 -0800

    MVP met.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,37 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import Card from "./Card";
+import styled from "styled-components";
+
+const WholePage = styled.div`
+  text-align: center;
+  display: flex;
+  flex-flow: column nowrap;
+  align-items: center;
+  background-color: lightblue;
+`;
+
+const Heading = styled.h1`
+  color: midnightblue;
+  background-color: skyblue;
+  padding: 0.5rem;
+  border: 1px dotted midnightblue;
+`;
 
 function App() {
-  const startDates=['2020-02-19', '2020-02-18', '2020-02-17'];
-  const [dates, setDates]=useState(startDates);
+  const startDates = ["2020-02-19", "2020-02-18", "2020-02-17"];
+  const [dates, setDates] = useState(startDates);
   return (
-    <div className="App">
+    <WholePage>
       <p>
-        Space time! <span role="img">🚀</span>
+        <Heading>
+          Space time! <span role="img">🚀</span>
+        </Heading>
       </p>
-      {dates.map((date,index) => {
-        return (<Card date={date} key={index}/>)
+      {dates.map((date, index) => {
+        return <Card date={date} key={index} />;
       })}
-    </div>
+    </WholePage>
   );
 }
 
--- src/Card.js
+++ src/Card.js
@@ -1,11 +1,44 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import "./Card.css";
+import styled from "styled-components";
+
+const CardStyle = styled.div`
+  display: flex;
+  flex-flow: column nowrap;
+  border: 1px midnightblue dotted;
+  background-color: ivory;
+  width: 75%;
+  align-items: center;
+  margin: 20px;
+`;
+
+const CardText = styled.p`
+  border: 1px midnightblue dotted;
+  margin: 3%;
+  background-color: rgb(255, 255, 245);
+`;
+
+const CardTitle = styled(CardText)`
+  padding: 0.7rem;
+`;
+
+const Image = styled.img`
+  max-width: 40%;
+  height: auto;
+`;
+
+const Description = styled(CardText)`
+  padding: 1rem;
+`;
 
 function Card(props) {
-    const [incoming, setIncoming]=useState({});
-    useEffect(()=>{
-      axios.get(`https://api.nasa.gov/planetary/apod?api_key=wz6guOf0CG6OCDt1i32EonxAgoxLb2dBGRbIAVwd&date=${props.date}`)
+  const [incoming, setIncoming] = useState({});
+  useEffect(() => {
+    axios
+      .get(
+        `https://api.nasa.gov/planetary/apod?api_key=wz6guOf0CG6OCDt1i32EonxAgoxLb2dBGRbIAVwd&date=${props.date}`
+      )
       .then(response => {
         setIncoming(response.data);
         console.log(response.data);
@@ -13,14 +46,14 @@ function Card(props) {
       .catch(error => {
         console.log("Nope. ", error);
       });
-    },[]);
-    return (
-      <div className="card">
-          <h2>{incoming.title}</h2>
-          <img src={incoming.url} />
-          <p>{incoming.explanation}</p>
-      </div>
-    );
-  }
-  
-  export default Card;
+  }, []);
+  return (
+    <CardStyle>
+      <CardTitle><h2>{incoming.title}</h2></CardTitle>
+      <Image src={incoming.url} />
+      <Description>{incoming.explanation}</Description>
+    </CardStyle>
+  );
+}
+
+export default Card;
